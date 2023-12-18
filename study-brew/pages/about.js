482fc7d9777a3674e25f31a81d70f6d830692639
Ryan Khalife <khaliferyan@gmail.com>
2023-12-18 17:47:09 -0500

    Create about section

--- study-brew/pages/about.js
+++ study-brew/pages/about.js
@@ -1,11 +1,29 @@
+import styles from "../styles/About.module.css";
+import Title from "../components/Title/Title";
+import Image from "next/image";
+import about from "../public/about.png";
+
 export default function About() {
   return (
-    <div>
-      <h1>About us</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste rerum quam cumque mollitia, libero quasi inventore enim assumenda
-        ullam eaque et repellat fuga illo error sit accusamus. Ipsa, minima mollitia!
-      </p>
-    </div>
+    <main className={styles.about}>
+      <section className={`${styles.aboutus}  container`}>
+        <Title title="About Us" />
+        <div className={styles.aboutContent}>
+          <div>
+            <Image src={about} width={500} height={500} />
+          </div>
+          <div>
+            <h2>Our Company</h2>
+            <p>
+              Nestled in the heart of a bustling college town, The Study Brew is more than just a coffee shop; it's a sanctuary for
+              students. Our founders, former students themselves, created this space for those seeking a blend of comfort, community, and
+              quality coffee. Here, late-night study sessions are fueled by artisanal blends, and every corner is tailored for both
+              collaborative and solitary pursuits. At The Study Brew, we're not just part of your daily caffeine routine; we're a chapter in
+              your academic journey.
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
