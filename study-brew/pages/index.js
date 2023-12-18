34fbe7a21cf58d49ebe52a801e68071d3312ea73
Ryan Khalife <khaliferyan@gmail.com>
2023-12-18 01:33:25 -0500

    Add hero section to home page

--- study-brew/pages/index.js
+++ study-brew/pages/index.js
@@ -1,5 +1,20 @@
 import { Button } from "@nextui-org/react";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className={styles.hero}>
+        <div className={`container ${styles.heroContainer}`}>
+          <p className={styles.heroText1}>
+            Fuel Your Success at The Study Brew: Enjoy Student Discounts on Specialty Coffee in Our Inspiring Study Space!
+          </p>
+          <Button size="lg" variant="shadow">
+            Redeem Your Free Coffee Today!
+          </Button>
+          <p className={styles.heroText2}>*Limited time offer till Dec31st- act now!</p>
+        </div>
+      </div>
+    </>
+  );
 }
