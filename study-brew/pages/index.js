f4a09f7a1f5bd6801daa562849288d8aaae799c9
Ryan Khalife <khaliferyan@gmail.com>
2023-12-18 03:58:56 -0500

    Add read more section to home page

--- study-brew/pages/index.js
+++ study-brew/pages/index.js
@@ -1,5 +1,7 @@
 import { Button } from "@nextui-org/react";
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import beans from "../public/beans.png";
 
 export default function Home() {
   return (
@@ -13,6 +15,19 @@ export default function Home() {
             Redeem Your Free Coffee Today!
           </Button>
           <p className={styles.heroText2}>*Limited time offer till Dec31st- act now!</p>
+        </div>
+      </div>
+      <div className={`${styles.readmore} container`}>
+        <Image src={beans} alt="Picture of dark roasted coffee beans" width={922} height={357} className={styles.readmoreImage} />
+        <div className={styles.readmoreText}>
+          <h2>Rich. Intense. Indulgent.</h2>
+          <p>
+            Enhanced by smoky and toasty undertones, our distinctive dark roasts embody our unique identity. Through our expert roasting
+            process, we unveil the profound, chocolatey sweetness of the beans, resulting in a decadently intricate coffee experience.
+          </p>
+          <Button size="lg" variant="shadow" color="warning">
+            Read More
+          </Button>
         </div>
       </div>
     </>
