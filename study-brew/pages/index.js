05b0374695647c05c7293debdfa47ac863b93999
Ryan Khalife <khaliferyan@gmail.com>
2023-12-18 05:14:31 -0500

    Styled menu card

--- study-brew/pages/index.js
+++ study-brew/pages/index.js
@@ -1,7 +1,8 @@
-import { Button } from "@nextui-org/react";
+import { Button, Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import beans from "../public/beans.png";
+import espresso from "../public/espresso.png";
 
 export default function Home() {
   return (
@@ -28,6 +29,27 @@ export default function Home() {
           <Button size="lg" variant="shadow" color="warning">
             Read More
           </Button>
+        </div>
+      </div>
+      <div className={styles.menu}>
+        <div className="container">
+          <h1>Popular Menu</h1>
+          <div className={styles.cardsContainer}>
+            <Card classNames={{ base: styles.card, header: styles.cardHeader, body: styles.cardBody }}>
+              <CardHeader>
+                <Image src={espresso} alt="Espresso" width={250} height={250} />
+              </CardHeader>
+              <CardBody>
+                <h3>Espresso</h3>
+                <p>
+                  Espresso is made by forcing nearly boiling water through finely-ground coffee beans, which results in a concentrated,
+                  syrup-like coffee drink.
+                </p>
+                <p>Ratings 5.0</p>
+                <p>+ JUST $1.99</p>
+              </CardBody>
+            </Card>
+          </div>
         </div>
       </div>
     </>
