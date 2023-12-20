7ba7e5c58cc0534d4684ea1f029dc879f5ab726f
Ryan Khalife <khaliferyan@gmail.com>
2023-12-20 01:01:18 -0500

    Add cookie consent banner component

--- study-brew/pages/_app.js
+++ study-brew/pages/_app.js
@@ -4,8 +4,17 @@ import Head from "next/head";
 import Navbar from "../components/Navbar/Navbar";
 import Script from "next/script";
 import Footer from "../components/Footer/Footer";
+import { CookieConsentBanner, triggerCookieConsentBanner } from "@porscheofficial/cookie-consent-banner-react";
 
 function MyApp({ Component, pageProps }) {
+  const initConsent = ({ acceptedCategories }) => {
+    if (acceptedCategories.includes("analytics")) {
+      ErrorTrackingService.init({
+        dsn: process.env.DSN,
+        environment: process.env.ENV,
+      });
+    }
+  };
   return (
     <>
       <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
@@ -28,6 +37,30 @@ function MyApp({ Component, pageProps }) {
         <Navbar />
         <Component {...pageProps} />
         <Footer />
+        <CookieConsentBanner
+          handlePreferencesUpdated={initConsent}
+          handlePreferencesRestored={initConsent}
+          btnLabelAcceptAndContinue="Agree and continue"
+          btnLabelOnlyEssentialAndContinue="Continue with technically required cookies only"
+          availableCategories={[
+            {
+              description: "Enable you to navigate and use the basic functions and to store preferences.",
+              key: "technically_required",
+              label: "Technically necessary cookies",
+              isMandatory: true,
+            },
+            {
+              description: "Enable us to determine how visitors interact with our service in order to improve the user experience.",
+              key: "analytics",
+              label: "Analysis cookies",
+            },
+          ]}
+        >
+          We use cookies and similar technologies to provide certain features, enhance the user experience and deliver content that is
+          relevant to your interests. Depending on their purpose, analysis and marketing cookies may be used in addition to technically
+          necessary cookies. By clicking on &quot;Agree and continue&quot;, you declare your consent to the use of the aforementioned
+          cookies.
+        </CookieConsentBanner>
       </NextUIProvider>
     </>
   );
