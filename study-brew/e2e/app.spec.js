9d766c20453f15c9258f11132a14a2347657d570
Bharadwaj-Sarikonda <ss799@njit.edu>
2023-12-20 17:13:26 -0500

    trail

--- study-brew/e2e/app.spec.js
+++ study-brew/e2e/app.spec.js
@@ -1,10 +1,3 @@
-import { test, expect } from "@playwright/test";
-
-test("title should be Study Brew", async ({ page }) => {
-  await page.goto("/");
-  expect(await page.title()).toBe("Study Brew");
-});
-
 const { test, expect } = require('@playwright/test');
 
 // Define the test suite
