ed2d0c001aa8163aa732ed4156da89282562c534
Ryan Khalife <khaliferyan@gmail.com>
2023-12-20 01:24:54 -0500

    Change GA code to use react-ga4

--- study-brew/pages/_app.js
+++ study-brew/pages/_app.js
@@ -5,6 +5,9 @@ import Navbar from "../components/Navbar/Navbar";
 import Script from "next/script";
 import Footer from "../components/Footer/Footer";
 import { CookieConsentBanner } from "@porscheofficial/cookie-consent-banner-react";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS);
 
 function MyApp({ Component, pageProps }) {
   const initConsent = ({ acceptedCategories }) => {
@@ -17,17 +20,6 @@ function MyApp({ Component, pageProps }) {
   };
   return (
     <>
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-        `}
-      </Script>
-
       <Head>
         <title>Study Brew</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
