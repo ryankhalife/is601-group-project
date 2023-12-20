d916f330f52bb18e9957478ec98bf3678aef63d1
Bharadwaj-Sarikonda <ss799@njit.edu>
2023-12-20 17:55:51 -0500

    just footer pt

--- study-brew/e2e/app.spec.js
+++ study-brew/e2e/app.spec.js
@@ -1,117 +1,39 @@
 const { test, expect } = require('@playwright/test');
 
-// Define the test suite
-test.describe('App Component Tests', () => {
-
-  // Check if the Navbar is rendered
-  test('Navbar is rendered', async ({ page }) => {
+test.describe('Footer Component Tests', () => {
+  // This will run before each test in this block
+  test.beforeEach(async ({ page }) => {
     await page.goto('https://study-brew.vercel.app/');
-    const navbar = await page.locator('text=Your Navbar Text Here'); // Replace with actual navbar text
-    await expect(navbar).toBeVisible();
+    await page.waitForLoadState('networkidle');
   });
 
-  // Check if the Footer is rendered
-test('Footer is rendered', async ({ page }) => {
-  await page.goto('https://study-brew.vercel.app/'); // Adjust the URL if needed
+  test('Privacy Policy link is rendered in the footer', async ({ page }) => {
+    // Assuming 'Privacy Policy' is unique text in the footer
+    const privacyPolicyLink = page.locator('text=Privacy Policy');
+    await expect(privacyPolicyLink).toBeVisible();
+    await expect(privacyPolicyLink).toHaveAttribute('href', '/privacy');
+  });
 
-  // Check for text that is unique to the footer
-  // You need to replace 'Privacy Policy' and 'Terms and Conditions' with the exact text you're using in your footer.
-  const privacyPolicyLink = page.locator('text=Privacy Policy');
-  const termsAndConditionsLink = page.locator('text=Terms and Conditions');
+  test('Terms and Conditions link is rendered in the footer', async ({ page }) => {
+    // Assuming 'Terms and Conditions' is unique text in the footer
+    const termsLink = page.locator('text=Terms and Conditions');
+    await expect(termsLink).toBeVisible();
+    await expect(termsLink).toHaveAttribute('href', '/terms');
+  });
 
-  // Verifying the footer sections are visible
-  await expect(privacyPolicyLink).toBeVisible();
-  await expect(termsAndConditionsLink).toBeVisible();
+  test('Home link is rendered in the footer', async ({ page }) => {
+    // Assuming 'Home' is unique text in the footer
+    const homeLink = page.locator('text=Home');
+    await expect(homeLink).toBeVisible();
+    await expect(homeLink).toHaveAttribute('href', '/');
+  });
+
+  test('About Us link is rendered in the footer', async ({ page }) => {
+    // Assuming 'About Us' is unique text in the footer
+    const aboutLink = page.locator('text=About Us');
+    await expect(aboutLink).toBeVisible();
+    await expect(aboutLink).toHaveAttribute('href', '/about');
+  });
+
+ 
 });
-
-  // Check if the Cookie Consent Banner is rendered
-  test('Cookie Consent Banner is rendered', async ({ page }) => {
-    await page.goto('https://study-brew.vercel.app/');
-    const consentBanner = await page.locator('text=Agree and continue');
-    await expect(consentBanner).toBeVisible();
-  });
-
-  // Check if the page title is "Study Brew"
-  test('Page title is correct', async ({ page }) => {
-    await page.goto('https://study-brew.vercel.app/');
-    await expect(page).toHaveTitle('Study Brew');
-  });
-});
-
-test.describe('About Page Tests', () => {
-
-    // Navigate to the About page before each test
-    test.beforeEach(async ({ page }) => {
-      await page.goto('https://study-brew.vercel.app/');
-    });
-  
-    // Check if the main title of the About page is present
-    test('Main title is rendered', async ({ page }) => {
-      const title = page.locator('text=About Us');
-      await expect(title).toBeVisible();
-    });
-  
-    // Check if the images are rendered
-    test('Images are rendered', async ({ page }) => {
-      const images = page.locator('img');
-      await expect(images).toHaveCount(6); // Number of images expected to render
-    });
-  
-    // Check if the press mentions are rendered
-    test('Press mentions are rendered', async ({ page }) => {
-      const pressItems = page.locator('text=Awarded Best Campus Coffee Shop');
-      await expect(pressItems).toBeVisible();
-    });
-  
-    // Check if the mission statement is present
-    test('Mission statement is rendered', async ({ page }) => {
-      const missionStatement = page.locator('text=Our Mission');
-      await expect(missionStatement).toBeVisible();
-    });
-  
-    // Check if the value & personality section is present
-    test('Value & Personality section is rendered', async ({ page }) => {
-      const valueSection = page.locator('text=Our Value & Personality');
-      await expect(valueSection).toBeVisible();
-    });
-  
-  });
-
-
-test.describe('Home Page Tests', () => {
-
-    // Navigate to the home page before each test
-    test.beforeEach(async ({ page }) => {
-      await page.goto('https://study-brew.vercel.app/');
-    });
-  
-    //Check if the hero section is rendered
-    test('Hero section is rendered', async ({ page }) => {
-      const heroSection = page.locator('section', { hasText: 'Fuel Your Success at The Study Brew' });
-      await expect(heroSection).toBeVisible();
-    });
-  
-    // Check if menu items are rendered
-    test('Menu items are rendered', async ({ page }) => {
-      const menuItems = page.locator('section', { hasText: 'Popular Menu' }).locator('img');
-      await expect(menuItems).toHaveCount(4); // Replace with the number of items you expect
-    });
-  
-    // Check if testimonials are rendered
-    test('Testimonials are rendered', async ({ page }) => {
-      const testimonials = page.locator('section', { hasText: 'Join 1,000+ scholars at their beloved Study Brew haven!' }).locator('img');
-      await expect(testimonials).toHaveCount(3); // Replace with the number of testimonials you expect
-    });
-  
-    // Check if the rewards section is rendered
-    test('Rewards section is rendered', async ({ page }) => {
-      const rewardsSection = page.locator('section', { hasText: 'Unlock your exclusive Brew Insider perks' });
-      await expect(rewardsSection).toBeVisible();
-    });
-  
-    // Check if the "Redeem Your Free Coffee Today!" button works
-    test('Free coffee redemption button is clickable', async ({ page }) => {
-      const redeemButton = page.locator('button', { hasText: 'Redeem Your Free Coffee Today!' });
-      await expect(redeemButton).toBeVisible();
-      await expect(redeemButton).toBeEnabled();
-    });})
